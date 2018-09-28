e5c01b0d92c3e2e833f703e3784de195241ce301
niharika4246 <43583157+niharika4246@users.noreply.github.com>
2018-09-28 21:33:10 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,29 @@
+#Q.1- Print something.
+print("Hello! We are here for learning Python")
+
+#Q.2- Concatinate Strings.
+s="Acad"+"View"
+print(s)
+
+#Q.3- User Defined variables.
+x=input('Enter the value of x : ')
+y=input('Enter the value of y : ')
+z=input('Enter the value of z : ')
+print(x,y,z)
+
+#Q.4- Print "Let's Get Started" on screen.
+print( "Let's Get Started")
+print( "Let\'s Get Started")
+print( "\"Let's Get Started\" ")
+
+#Q.5- Print the given values using placeholders.
+s="Acadview"
+course="Python"
+fees=5000
+print("%s %s %d"%(s,course,fees))
+
+#Q.6- Find the area of circle.
+pi=3.14
+r=float(input("Enter the radius : "))
+print(pi*r*r)
+print(pi*r**2)
