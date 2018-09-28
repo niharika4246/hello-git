bfe700cfd27b0e9b212afdfa6c597b09f185f93e
niharika4246 <sharmaniharika94182@gmail.com>
2018-09-28 15:07:38 +0530

    first commit

---
+++ git commands.py
@@ -0,0 +1,10 @@
+#to create a new repository 
+
+
+git init
+git config --global user.name "Niharika4246"
+git config --global user.email "sharmaniharika94182@gmail.com"
+git add 'file name'
+git commit -m "first commit"
+git remote add origin https://github.com/niharika4246/Shoolini.git
+git push -u origin master
